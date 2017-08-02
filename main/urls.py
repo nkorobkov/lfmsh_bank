67731963f79bfb6df34708ee33337b3c9078c3f2
Nikita Korobkov <nikkorobk@gmail.com>
2017-08-02 21:30:05 +0300

    urls fix

--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
     url(r'^bank/', include('bank.urls', namespace='bank')),
-    url(r'^bank_api/', include('bank_api.urls', namespace='bank_api'))
+    url(r'^bank_api/', include('bank_api.urls', namespace='bank_api')),
+    url(r'^$', include('bank.urls', namespace='bank'))
 
 ]

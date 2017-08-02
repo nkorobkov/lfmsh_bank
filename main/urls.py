9a27d80af38ee432220719db5763a970560c00b4
Nikita Korobkov <nikkorobk@gmail.com>
2017-08-02 22:00:56 +0300

    urls fix

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bank/', include('bank.urls', namespace='bank')),
     url(r'^bank_api/', include('bank_api.urls', namespace='bank_api')),
-    url(r'^$', include('bank.urls', namespace='bank'))
 
 ]

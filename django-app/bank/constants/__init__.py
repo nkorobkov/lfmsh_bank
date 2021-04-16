e1a438f612f43b375a3c8af4cd656cd2930993c0
Nikita Korobkov <nikkorobk@gmail.com>
2021-04-16 18:55:13 -0400

    rename with underscores to see if it helps ci

--- django-app/bank/constants/__init__.py
+++ django-app/bank/constants/__init__.py
@@ -4,5 +4,5 @@ from .attendance_type_enum import AttendanceTypeEnum
 from .attendance_block_enum import AttendanceBlockEnum
 from .transaction_type_enum import TransactionTypeEnum
 from .money_type_enum import MoneyTypeEnum
-from .states import States
-from .actions import Actions
+from .states_enum import States
+from .actions_enum import Actions
